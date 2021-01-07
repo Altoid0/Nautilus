45a9724cc5ffb71c91a4b176b2029a2000a7ea36
DhruvK0 <46884021+DhruvK0@users.noreply.github.com>
2021-01-06 18:20:30 -0800

    pls work

---
+++ src/test/java/com/example/Nautilus/NautilusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Nautilus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NautilusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
