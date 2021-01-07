2325286c2e7a36d0f5c262c1211d4e62c322066f
Tanay Shah <tanay.v.shah@gmail.com>
2021-01-07 00:40:22 -0800

    simply the best

--- menu/src/main/java/NautilusApplication.java
+++ menu/src/main/java/NautilusApplication.java
@@ -1,4 +1,4 @@
-package com.example.Nautilus;
+//package com.example.Nautilus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
