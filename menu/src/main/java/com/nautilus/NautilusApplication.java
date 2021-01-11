b5bc7967a2bef9ab8121c14bc3c2130c368aea31
dhruv <dhruv.sengupta04@gmail.com>
2021-01-11 08:59:18 -0800

    Adde thyme for html for NautilusApplication

--- menu/src/main/java/com/nautilus/NautilusApplication.java
+++ menu/src/main/java/com/nautilus/NautilusApplication.java
@@ -2,9 +2,16 @@ package com.nautilus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+//@Controller
 @SpringBootApplication
 public class NautilusApplication {
+	@RequestMapping(value = "/index")
+	public String index() {
+		return "index";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NautilusApplication.class, args);
