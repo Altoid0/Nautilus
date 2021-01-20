43873965366a8a5716f96e750d76c56e9686bea8
Tanay Shah <tanay.v.shah@gmail.com>
2021-01-20 08:50:30 -0800

    working runtime and compile

--- menu/src/main/java/com/nautilus/NautilusApplication.java
+++ menu/src/main/java/com/nautilus/NautilusApplication.java
@@ -1,5 +1,6 @@
 package com.nautilus;
 
+import com.calendar.CalendarRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
