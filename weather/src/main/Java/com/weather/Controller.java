bb8f3db7a8c507db6326ab7547c1d5e6c8647402
DhruvK0 <46884021+DhruvK0@users.noreply.github.com>
2021-02-04 21:04:52 -0800

    cleaned up uneeded files

--- weather/src/main/Java/com/weather/Controller.java
+++
@@ -1,9 +0,0 @@
-package com.weather;
-
-
-public class Controller {
-    public static void main(String[] args) {
-        ApiCall test = new ApiCall("Cupertino");
-        System.out.print(test.returnFeels());
-    }
-}
