04867c56ddb4f3de90a6b0762171581c5eacde65
DhruvK0 <46884021+DhruvK0@users.noreply.github.com>
2021-02-04 21:04:13 -0800

    fixed index and weather html files

--- weather/src/main/Java/com/weathercall/Weather.java
+++ weather/src/main/Java/com/weathercall/Weather.java
@@ -1,4 +1,5 @@
-//import com.weather.ApiCall;
+package com.weathercall;//package com.weather;
+//import com.nautilus.ApiCall;
 //import org.springframework.stereotype.Controller;
 //import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.*;
@@ -6,25 +7,25 @@
 //
 //
 //
-//public class Weather {
+//public class com.weather.com.weathercall.Weather {
 //
 //    @RequestMapping(value ="/weather", method=RequestMethod.GET)
 //    public String currentTemp (Model model) {
-//        ApiCall test = new ApiCall("Los Angeles");
+//        com.nautilus.ApiCall test = new com.nautilus.ApiCall("Los Angeles");
 //        String current_temp = test.returnCurrent().toString();
 //        model.addAttribute("current_temp", current_temp);
 //        return "weather.html";
 //    }
 //}
-import com.weather.ApiCall;
+
 
 public class Weather {
     public static void main(String[] args) {
-        ApiCall test = new ApiCall("Los Angeles");
-        String feels = test.returnFeels().toString();
-        System.out.println(feels);
-        System.out.println(test.returnFeels());
-        System.out.println(test.returnForecast());
+//        ApiCall test = new ApiCall("Los Angeles");
+//        String feels = test.returnFeels().toString();
+//        System.out.println(feels);
+//        System.out.println(test.returnFeels());
+//        System.out.println(test.returnForecast());
 
 
     }
